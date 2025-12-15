2d17040dde2123a1a2e4253004bce93dd1ab60e3
HanamileH <hanamileh@gmail.com>
2025-12-15 10:59:30 +0300

    Restructure fill tables script

--- app/sql/fill_tables.sql
+++ app/sql/fill_tables.sql
@@ -1,6 +1,14 @@
 -- Postgresql 18
 
 -- ===== Код для заполнения таблиц тестовыми данными ===== --
+-- Заполнение таблицы типов кузова
+INSERT INTO body_type (name) VALUES
+('sedan'),
+('SUV'),
+('hatchback'),
+('coupe'),
+('convertible'),
+('wagon');
 
 -- Заполнение таблицы брендов (5 марок)
 INSERT INTO brand (name) VALUES
@@ -18,88 +26,72 @@ INSERT INTO color (name, hex_code) VALUES
 ('Красный', 'FF0000'),
 ('Синий', '0000FF');
 
--- Заполнение таблицы моделей (10 моделей)
-INSERT INTO model (name, image_path, brand_id, year, engine_type, engine_volume, engine_power, transmission, body_type) VALUES
--- Audi
-('A4', 'audi_a4.png', 1, 2023, 'petrol', 2.0, 190, 'automatic', 'sedan'),
-('Q5', 'audi_q5.png', 1, 2023, 'diesel', 3.0, 286, 'automatic', 'SUV'),
--- BMW
-('3 Series', 'bmw_3series.png', 2, 2023, 'petrol', 2.0, 184, 'automatic', 'sedan'),
-('X5', 'bmw_x5.png', 2, 2023, 'hybrid', 3.0, 394, 'automatic', 'SUV'),
--- Mercedes-Benz
-('C-Class', 'mercedes_cclass.png', 3, 2023, 'petrol', 2.0, 258, 'automatic', 'sedan'),
-('GLE', 'mercedes_gle.png', 3, 2023, 'diesel', 2.0, 245, 'automatic', 'SUV'),
--- Toyota
-('Camry', 'toyota_camry.png', 4, 2023, 'hybrid', 2.5, 218, 'automatic', 'sedan'),
-('RAV4', 'toyota_rav4.png', 4, 2023, 'petrol', 2.0, 150, 'manual', 'SUV'),
--- Volkswagen
-('Passat', 'vw_passat.png', 5, 2023, 'petrol', 1.5, 150, 'automatic', 'sedan'),
-('Tiguan', 'vw_tiguan.png', 5, 2023, 'diesel', 2.0, 150, 'automatic', 'SUV');
+-- Заполнение таблицы моделей автомобилей (10 моделей)
+INSERT INTO model (
+   name, 
+   description, 
+   image_path, 
+   brand_id, 
+   price, 
+   year, 
+   engine_type, 
+   engine_volume, 
+   engine_power, 
+   transmission, 
+   body_type
+) VALUES
+-- Audi (id: 1)
+('A4', 'Современный седан премиум-класса с передовыми технологиями', 'audi_a4.png', 1, 3500000, 2023, 'petrol', 2.0, 190, 'automatic', 1),
+('Q5', 'Вместительный кроссовер с полным приводом и комфортным салоном', 'audi_q5.png', 1, 4500000, 2023, 'diesel', 3.0, 286, 'automatic', 2),
 
--- Заполнение таблицы конфигураций моделей (15 конфигураций)
-INSERT INTO model_configuration (name, description, model_id, price) VALUES
--- Audi A4 конфигурации
-('Standard', 'Базовая комплектация с кожаным салоном и мультимедиа', 1, 3500000.00),
-('Premium', 'Премиум комплектация с панорамной крышей и ксеноном', 1, 4200000.00),
-('Sport', 'Спортивная комплектация с улучшенной подвеской', 1, 3900000.00),
+-- BMW (id: 2)
+('3 Series', 'Легендарный спортивный седан с отличной управляемостью', 'bmw_3series.png', 2, 3800000, 2023, 'petrol', 2.0, 184, 'automatic', 1),
+('X5', 'Люксовый внедорожник с гибридной силовой установкой', 'bmw_x5.png', 2, 5500000, 2023, 'hybrid', 3.0, 394, 'automatic', 2),
 
--- Audi Q5 конфигурации
-('Comfort', 'Комфортная комплектация для городской езды', 2, 4500000.00),
-('Luxury', 'Люксовая комплектация с массой опций', 2, 5200000.00),
+-- Mercedes-Benz (id: 3)
+('C-Class', 'Элегантный бизнес-седан с премиальным интерьером', 'mercedes_cclass.png', 3, 4100000, 2023, 'petrol', 2.0, 258, 'automatic', 1),
+('GLE', 'Роскошный внедорожник для города и бездорожья', 'mercedes_gle.png', 3, 5200000, 2023, 'diesel', 2.0, 245, 'automatic', 2),
 
--- BMW 3 Series конфигурации
-('Modern', 'Современная комплектация с цифровыми приборами', 3, 3800000.00),
-('M Sport', 'Спортивный пакет M с аэродинамическим обвесом', 3, 4300000.00),
+-- Toyota (id: 4)
+('Camry', 'Надежный семейный седан с гибридной установкой', 'toyota_camry.png', 4, 2500000, 2023, 'hybrid', 2.5, 218, 'automatic', 1),
+('RAV4', 'Популярный кроссовер с отличной проходимостью', 'toyota_rav4.png', 4, 3200000, 2023, 'petrol', 2.0, 150, 'manual', 2),
 
--- BMW X5 конфигурации
-('xLine', 'Комфортная комплектация для семьи', 4, 5500000.00),
-('M Package', 'Спортивный пакет с улучшенными характеристиками', 4, 6200000.00),
-
--- Mercedes C-Class конфигурации
-('Avantgarde', 'Стильная комплектация с премиум опциями', 5, 4100000.00),
-('AMG Line', 'Спортивный пакет AMG с уникальным дизайном', 5, 4700000.00),
-
--- Toyota Camry конфигурации
-('Comfort', 'Комфортная комплектация для ежедневных поездок', 7, 2500000.00),
-('Premium', 'Премиум комплектация с кожаным салоном', 7, 2900000.00),
-
--- Volkswagen Passat конфигурации
-('Trendline', 'Базовая комплектация с необходимыми опциями', 9, 2200000.00),
-('Highline', 'Топовая комплектация с полным набором опций', 9, 2800000.00),
-('R-Line', 'Спортивная комплектация с элементами R-Line', 9, 2600000.00);
+-- Volkswagen (id: 5)
+('Passat', 'Практичный бизнес-седан немецкого качества', 'vw_passat.png', 5, 2200000, 2023, 'petrol', 1.5, 150, 'automatic', 1),
+('Tiguan', 'Стильный городской кроссовер для всей семьи', 'vw_tiguan.png', 5, 2800000, 2023, 'diesel', 2.0, 150, 'automatic', 2);
 
 -- Заполнение таблицы автомобилей (20 автомобилей)
-INSERT INTO car (model_configuration_id, vin, color_id, status) VALUES
--- Audi A4
-(1, 'WAUZZZ8KZFA123456', 1, 'available'),
-(2, 'WAUZZZ8KZFA123457', 2, 'sold'),
-(3, 'WAUZZZ8KZFA123458', 3, 'available'),
+INSERT INTO car (model_id, vin, color_id) VALUES
+-- Audi A4 (model_id: 1)
+(1, 'WAUZZZ8KZFA123456', 1),
+(1, 'WAUZZZ8KZFA123457', 2),
+(1, 'WAUZZZ8KZFA123458', 3),
 
--- Audi Q5
-(4, 'WA1VAAF75HD123459', 4, 'reserved'),
-(5, 'WA1VAAF75HD123460', 1, 'available'),
+-- Audi Q5 (model_id: 2)
+(2, 'WA1VAAF75HD123459', 4),
+(2, 'WA1VAAF75HD123460', 1),
 
--- BMW 3 Series
-(6, 'WBA8E9C58JEU12361', 2, 'available'),
-(7, 'WBA8E9C58JEU12362', 5, 'sold'),
+-- BMW 3 Series (model_id: 3)
+(3, 'WBA8E9C58JEU12361', 2),
+(3, 'WBA8E9C58JEU12362', 5),
 
--- BMW X5
-(8, '5UXCR6C56KUL12363', 3, 'available'),
-(9, '5UXCR6C56KUL12364', 4, 'available'),
+-- BMW X5 (model_id: 4)
+(4, '5UXCR6C56KUL12363', 3),
+(4, '5UXCR6C56KUL12364', 4),
 
--- Mercedes C-Class
-(10, 'WDDWF4EB3FN123465', 1, 'reserved'),
-(11, 'WDDWF4EB3FN123466', 2, 'available'),
+-- Mercedes C-Class (model_id: 5)
+(5, 'WDDWF4EB3FN123465', 1),
+(5, 'WDDWF4EB3FN123466', 2),
 
--- Toyota Camry
-(12, 'JTNK4JEC0M2123467', 3, 'available'),
-(13, 'JTNK4JEC0M2123468', 5, 'sold'),
+-- Toyota Camry (model_id: 7)
+(7, 'JTNK4JEC0M2123467', 3),
+(7, 'JTNK4JEC0M2123468', 5),
 
--- Volkswagen Passat
-(14, 'WVWZZZ3CZJE123469', 1, 'available'),
-(15, 'WVWZZZ3CZJE123470', 2, 'available'),
-(14, 'WVWZZZ3CZJE123471', 4, 'reserved'),
-(15, 'WVWZZZ3CZJE123472', 3, 'available'),
-(14, 'WVWZZZ3CZJE123473', 5, 'sold'),
-(15, 'WVWZZZ3CZJE123474', 1, 'available'),
-(14, 'WVWZZZ3CZJE123475', 2, 'available');
+-- Volkswagen Passat (model_id: 9)
+(9, 'WVWZZZ3CZJE123469', 1),
+(9, 'WVWZZZ3CZJE123470', 2),
+(9, 'WVWZZZ3CZJE123471', 4),
+(9, 'WVWZZZ3CZJE123472', 3),
+(9, 'WVWZZZ3CZJE123473', 5),
+(9, 'WVWZZZ3CZJE123474', 1),
+(9, 'WVWZZZ3CZJE123475', 2);
